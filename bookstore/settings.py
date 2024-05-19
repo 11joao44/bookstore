e97eacc2c27262c55ea5269d960c1beab9f0bcd3
Yasmin Melo <carol721@gmail.com>
2024-05-19 08:06:59 -0400

    Correção Split

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -162,5 +162,5 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-print(os.environ.get("DJANGO_ALLOWED_HOSTS"))
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost").split(" ")
+
