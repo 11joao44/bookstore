a5abe104a2e036cc14cf569f6b757b3759969bb7
Yasmin Melo <carol721@gmail.com>
2024-05-23 00:05:06 -0400

    DB config

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         "NAME": os.environ.get("SQL_DATABASE", "bookstore_dev_db"),
         "USER": os.environ.get("SQL_USER", "bookstore_dev"),
         "PASSWORD": os.environ.get("SQL_PASSWORD", "bookstore_dev"),
-        "HOST": os.environ.get("SQL_HOST", "localhost"),  # ou "db" se estiver usando Docker
+        "HOST": os.environ.get("SQL_HOST", "db"),
         "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
