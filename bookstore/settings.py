6434034896d400b15147c08f5a9d182f14e5c3b0
Yasmin Melo <carol721@gmail.com>
2024-06-03 20:04:02 -0400

    Chore: Configure DEBUG using environment variable in settings.py

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -152,6 +152,10 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
+DEBUG_TOOLBAR_CONFIG = {
+    'IS_RUNNING_TESTS': False
+}
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
